a31c0c55e7f7caa64673003b44a8d76712f1f52a
Avram86 <60349668+Avram86@users.noreply.github.com>
2021-04-26 18:57:42 +0300

    Count no of vowels

--- String-exercises/Program.cs
+++ String-exercises/Program.cs
@@ -6,7 +6,46 @@ namespace String_exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CountVowels();
+        }
+
+        private static void CountVowels()
+        {
+            /*
+             citim un string de la tastatura
+            cate vocale are stringul*/
+
+            Console.Write("Please enter the text: ");
+            string text = Console.ReadLine();
+            text = text.ToUpper();
+
+            int nrVocale = 0;
+
+            for(int i = 0; i < text.Length; i++)
+            {
+                switch (text[i])
+                {
+                    case 'A':
+                        nrVocale++;
+                        break;
+                    case 'E':
+                        nrVocale++;
+                        break;
+                    case 'I':
+                        nrVocale++;
+                        break;
+                    case 'O':
+                        nrVocale++;
+                        break;
+                    case 'U':
+                        nrVocale++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine($"Nr de vocale din text este de : {nrVocale}");
+
         }
     }
 }

683dde1fdd28bc8840498eb7dc080304b27d32ce
Avram86 <60349668+Avram86@users.noreply.github.com>
2021-04-26 19:35:18 +0300

    Reversed text

--- String-exercises/Program.cs
+++ String-exercises/Program.cs
@@ -6,7 +6,7 @@ namespace String_exercises
     {
         static void Main(string[] args)
         {
-            CountVowels();
+            Reverse("Index");
         }
 
         private static void CountVowels()
@@ -23,6 +23,12 @@ namespace String_exercises
 
             for(int i = 0; i < text.Length; i++)
             {
+                char c = text[i];
+
+                //verificam daca este cifra
+                //bool isDigit = char.IsDigit(c);
+
+
                 switch (text[i])
                 {
                     case 'A':
@@ -46,6 +52,55 @@ namespace String_exercises
             }
             Console.WriteLine($"Nr de vocale din text este de : {nrVocale}");
 
+        }
+
+        private static bool IsNumber(string text)
+        {
+            /*considerand un text dat, intoarcem un flag care ne spune daca textul e numar sau nu*/
+
+            bool isNumber =int.TryParse(text, out int result);
+            //eroare daca textul depaseste int.maxValue
+            //varianta:
+            //bool isNumber =long.TryParse(text, out long result);
+
+
+            foreach (char c in text)
+            {
+                if (!char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+        private static void ReadNumber(string label ,int maxAttempts, int defaultValue)
+        {
+            /*Read a text from console, and validate that it is a number
+             If not a number, repeat read(maxAttempts count)
+            If maxAttempts exceeded, return default*/
+
+            Console.Write(label);
+            string text = Console.ReadLine();
+            bool isNumber = IsNumber(text);
+
+            
+        }
+
+        private static string Reverse(string text)
+        {
+            char[] reversed = new char[text.Length];
+
+            for(int i = 0; i < text.Length; i++)
+            {
+                reversed[i] = text[text.Length - 1 - i];
+            }
+
+            foreach(var c in reversed)
+            {
+                Console.Write(c);
+            }
+
+            return reversed.ToString();
         }
     }
 }
